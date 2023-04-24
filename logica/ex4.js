419f0f19e49032ac9085f9156b3a619101dd4838
Guilherme Almeida <guilherme.dev.almeida@gmail.com>
2023-04-24 15:08:19 -0300

    feat: ex4.js
    Agora irá aparecer um alert na tela, tanto ao clicar no nome do signo, quanto na sua imagem.

--- logica/ex4.js
+++ logica/ex4.js
@@ -1,6 +1,15 @@
-const signs = document.querySelectorAll('.sign-name');
-signs.forEach(sign => {
-    sign.addEventListener('click', function showSign() {
+const signImage = document.querySelectorAll('.horoscope-info');
+const signsText = document.querySelectorAll('.sign-name');
+
+signImage.forEach(sign => {
+    sign.addEventListener('click', () => {
+        const dataId = sign.dataset.id;
+        alert(`Você clicou no signo de: ${dataId}`);
+    });
+});
+
+signsText.forEach(sign => {
+    sign.addEventListener('click', () => {
         const signName = sign.textContent;
         alert(`Você clicou no signo de: ${signName}`);
     });
