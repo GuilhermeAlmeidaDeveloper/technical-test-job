ef7f2c823756df99aea00d6ba23119cccfb2cc19
Guilherme Almeida <guilherme.dev.almeida@gmail.com>
2023-04-24 00:54:12 -0300

    fixed: ex1a.js fixed

--- funis-de-compra/ex1a.js
+++ funis-de-compra/ex1a.js
@@ -1,17 +1,21 @@
-function categoryStep() {
+function productStep() {
     let itensLength = 0;
     let productIDs = [];
+    let produtos = [];
 
     for (let i = 0; i < dataLayer.length; i++) {
         if (dataLayer[i].ecommerce) {
-            const products = dataLayer[i].ecommerce.impressions;
-            itensLength += products.length;
-            for (let product of products) {
-                productIDs.push(product.id);
-            };
+            produtos = dataLayer[i].ecommerce.impressions;
         };
     };
+
+    for (const produto of produtos) {
+        productIDs.push(produto.id)
+    }
+
+    itensLength = produtos.length
+
     console.log(`Lista de ID's dos produtos exibidos: ${productIDs.toLocaleString()}`);
     console.log(`Total de itens exibidos: ${itensLength}`);
 };
-categoryStep();
+productStep();
