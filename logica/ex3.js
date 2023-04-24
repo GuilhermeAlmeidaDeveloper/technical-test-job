9f58b8232ebbce093e84889642c50f6072fdc0d6
Guilherme Almeida <guilherme.dev.almeida@gmail.com>
2023-04-23 23:53:55 -0300

    fixed: fixed ex3.js

--- logica/ex3.js
+++ logica/ex3.js
@@ -1,5 +1,5 @@
-const divsAreaList = document.querySelectorAll('.ad-area');
-const divsAreaArray = Array.from(divsAreaList);
+let divsAreaList = document.querySelectorAll('.ad-area');
+let divsAreaArray = Array.from(divsAreaList);
 
 const url = window.location.href;
 const splitUrl = url.split(".br/");
@@ -13,17 +13,20 @@ function urlVerification(url) {
     if (url.includes('/home') || url === 'https://www.band.uol.com.br/') {
         console.log('Você está na categoria: Home');
         !divsAreaList ? console.log(divsNotFoundedMsg) : console.log(divsAreaArray);
-    };
-    console.log(`Você está na categoria: ${category}`);
-    if (url.includes('/esportes')) {
-        !divsAreaList ? console.log(divsNotFoundedMsg) : console.log(divsAreaArray);
-    };
-    if (url.includes('/entretenimento')) {
-        !divsAreaList ? console.log(divsNotFoundedMsg) : console.log(divsAreaArray);
-    };
-    if (url.includes('/noticias')) {
-        !divsAreaList ? console.log(divsNotFoundedMsg) : console.log(divsAreaArray);
-    };
+    } else {
+        console.log(`Você está na categoria: ${category}`);
+        if (url.includes('/esportes')) {
+            !divsAreaList ? console.log(divsNotFoundedMsg) : console.log(divsAreaArray);
+        };
+        if (url.includes('/entretenimento')) {
+            !divsAreaList ? console.log(divsNotFoundedMsg) : console.log(divsAreaArray);
+        };
+        if (url.includes('/noticias')) {
+            !divsAreaList ? console.log(divsNotFoundedMsg) : console.log(divsAreaArray);
+        };
+
+    }
+
 };
 urlVerification(url);
 
