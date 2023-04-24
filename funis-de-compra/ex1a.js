7fd412b695aceb1718f0d7e1b4dabc337f5010a7
Guilherme Almeida <guilherme.dev.almeida@gmail.com>
2023-04-24 02:14:28 -0300

    fixed: putting the variables in english

--- funis-de-compra/ex1a.js
+++ funis-de-compra/ex1a.js
@@ -1,17 +1,17 @@
 function productStep() {
     let itensLength = 0;
     let productIDs = [];
-    let produtos = [];
+    let products = [];
 
     for (let i = 0; i < dataLayer.length; i++) {
         if (dataLayer[i].ecommerce) {
-            produtos = dataLayer[i].ecommerce.impressions;
+            products = dataLayer[i].ecommerce.impressions;
         };
     };
-    for (const produto of produtos) {
+    for (const produto of products) {
         productIDs.push(produto.id);
     }
-    itensLength = produtos.length;
+    itensLength = products.length;
     console.log(`Lista de ID's dos produtos exibidos: ${productIDs.toLocaleString()}`);
     console.log(`Total de itens exibidos: ${itensLength}`);
 };
